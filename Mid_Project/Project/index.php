f59ccaa9fe3c16fc2c1d7207660234d77b93b010
kabirsadman <sadmankabirsami9@gmail.com>
2021-11-02 01:33:01 +0600

    Mid_Project

---
+++ Mid_Project/Project/index.php
@@ -0,0 +1,232 @@
+<?php
+session_start();
+if(isset($_SESSION['flag'])){
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+    </head>
+    <body>
+
+        
+            
+                
+                    <input type="text" placeholder="Search Product" name="">
+                    <button>Search</button><br><br>
+                    <a href="cart.html"><button>View Cart</button></a>
+                    <a href="compare.html"><button>Compare Demo</button></a>
+                      <a href="member_card.html"><button>View Member Card</button></a>
+                     <a href="logout.php"><button>Logout</button></a>
+                     
+                
+                
+                <h1 >Special Shoes With Offers</h1>
+             
+                
+                   
+                    
+                                <img src = "images/shoe-1.png" alt = "product image" style="width: 40%;">
+                            <br>
+                                 <a href="cart.html"><button>Add to Cart</button></a>
+
+                                 <a href="cart.html"><button>Buy now</button></a>
+
+                               <a href="product1.html"> <button type = "button" >Details
+                                </button>
+                                </a>
+                                <br>
+                                
+
+                                <br><br><label for="feedback">Give your feedback</label>
+                                <br><textarea name="feedback" id="lol" placeholder="Your comment"></textarea><br>
+                                <a href="feedaback.html"><button>Submit</button></a>
+                             
+                                <h2 >Good</h2>
+                                
+                            
+                       
+                            <p >$ 100.00</p>
+                            <p >$ 50.00</p>
+                        
+                            <h2 >50% off</h2>
+                        
+                                <img src = "images/shoe-2.png" alt = "product image" style="width: 40%;">
+                            <br>
+                                 <a href="cart.html"><button>Add to Cart</button></a>
+
+                                 <a href="cart.html"><button>Buy now</button></a>
+
+                               <a href="product2.html"> <button type = "button" >Details
+                                </button>
+                                </a>
+                                <br>
+                                
+                                 <br><br><label for="feedback">Give your feedback</label>
+                                <br><textarea name="feedback" id="lol" placeholder="Your comment"></textarea><br>
+                                <a href="feedaback.html"><button>Submit</button></a>
+                            
+                                <h2 >Good</h2>
+                            
+                           
+                            <p >$ 100.00</p>
+                            <p >$ 50.00</p>
+                        
+                                <img src = "images/shoe-3.png" alt = "product image" style="width: 40%;">
+                            <br>
+                                <a href="cart.html"><button>Add to cart</button></a>
+
+                                 <a href="cart.html"><button>Buy now</button></a>
+
+                                <a href="product3.html"> <button type = "button" >Details
+                                </button>
+                                </a>
+                                <br>
+                                
+                                 <br><br><label for="feedback">Give your feedback</label>
+                                <br><textarea name="feedback" id="lol" placeholder="Your comment"></textarea><br>
+                                <a href="feedaback.html"><button>Submit</button></a>
+                            
+                                <h2 >Good</h2>
+                             
+                        
+                            <p >$ 100.00</p>
+                            <p >$ 50.00</p>
+                        
+                                <img src = "images/shoe-4.png" alt = "product image" style="width: 40%;">
+                            <br>
+                                <a href="cart.html"><button>Add to cart</button></a>
+
+                                 <a href="cart.html"><button>Buy now</button></a>
+
+                                <a href="product4.html"> <button type = "button">Details
+                                </button>
+                                </a>
+                                <br>
+                                
+                                 <br><br><label for="feedback">Give your feedback</label>
+                                <br><textarea name="feedback" id="lol" placeholder="Your comment"></textarea><br>
+                                <a href="feedaback.html"><button>Submit</button></a>
+                            
+                                <h2 >Good</h2>
+                                
+                            
+                            <p >$ 100.00</p>
+                            <p >$ 50.00</p>
+                        
+                            <h2 >50% off</h2>
+                       
+                                <img src = "images/shoe-5.png" alt = "product image" style="width: 40%;">
+                            <br>
+                                 <a href="cart.html"><button>Add to cart</button></a>
+
+                                 <a href="cart.html"><button>Buy now</button></a>
+
+                                <a href="product5.html"> <button type = "button" >Details
+                                </button>
+                                </a>
+                                <br>
+                                
+                                 <br><br><label for="feedback">Give your feedback</label>
+                                <br><textarea name="feedback" id="lol" placeholder="Your comment"></textarea><br>
+                                <a href="feedaback.html"><button>Submit</button></a>
+                            
+                                <h2 >Good</h2>
+                            
+                            <p >$ 100.00</p>
+                            <p >$ 50.00</p>
+                        
+                    
+                                <img src = "images/shoe-6.png" alt = "product image" style="width: 40%;">
+                            <br>
+                                 <a href="cart.html"><button>Add to cart</button></a>
+
+                                <a href="cart.html"><button>Buy now</button></a>
+
+                                <a href="product6.html"> <button type = "button" >Details
+                                </button>
+                                </a>
+                                <br>
+                                
+                                 <br><br><label for="feedback">Give your feedback</label>
+                                <br><textarea name="feedback" id="lol" placeholder="Your comment"></textarea><br>
+                                <a href="feedaback.html"><button>Submit</button></a>
+                            
+                                <h2 >Good</h2>
+                             
+                            <p >$ 100.00</p>
+                            <p >$ 50.00</p>
+                       
+                    
+                                <img src = "images/shoe-7.png" alt = "product image" style="width: 40%;">
+                            <br>
+                                 <a href="cart.html"><button>Add to cart</button></a>
+
+                                <a href="cart.html"><button>Buy now</button></a>
+
+                                <a href="product7.html"> <button type = "button" >Details
+                                </button>
+                                </a>
+                                <br>
+                                
+                                 <br><br><label for="feedback">Give your feedback</label>
+                                <br><textarea name="feedback" id="lol" placeholder="Your comment"></textarea><br>
+                                <a href="feedaback.html"><button>Submit</button></a>
+                            
+
+                                <h2>Good</h2>
+                            
+                            <p >$ 100.00</p>
+                            <p >$ 50.00</p>
+                       
+                                <img src = "images/shoe-8.png" alt = "product image" style="width: 40%;">
+                            <br>
+                                 <a href="cart.html"><button>Add to cart</button></a>
+
+                                 <a href="cart.html"><button>Buy now</button></a>
+
+                                <a href="product8.html"> <button type = "button" >Details
+                                </button>
+                                </a>
+                                <br>
+                                
+                                 <br><br><label for="feedback">Give your feedback</label>
+                                <br><textarea name="feedback" id="lol" placeholder="Your comment"></textarea><br>
+                                <a href="feedaback.html"><button>Submit</button></a>
+                            
+
+                        
+                                <h2 >Good</h2>
+                                
+                            
+                            <p >$ 100.00</p>
+                            <p >$ 50.00</p>
+                        
+
+                        
+                            <h2 >50% off</h2>
+                        
+                    
+                    
+                
+            
+        </div>
+    </body>
+</html>
+
+<?php
+
+ }
+ else{
+    header('location:login.html');
+ }
+
+
+ ?>
+    
